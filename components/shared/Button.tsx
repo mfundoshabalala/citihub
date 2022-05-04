410adbdf20511a5f2fbec8872b37d84c9f6ac2c9
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-04 14:56:48 +0200

    Button.tsx update

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -11,7 +11,7 @@ const Button: ButtonInterface = ({ title, type = 'button', className, onClick })
 	return (
 		<button
 			type={type}
-			className={`${className} px-8 py-2 border-0 rounded shadow active:shadow-none max-w-xs`}
+			className={`${className} drop-shadow px-8 py-2 border-0 rounded shadow active:shadow-none max-w-xs`}
 			onClick={onClick}
 		>
 			{title}
