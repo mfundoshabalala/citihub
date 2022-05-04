f4c719920a37652516abb2509703ec9fdb5b759e
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-04 14:56:29 +0200

    icon.tsx update

--- components/shared/Icon.tsx
+++ components/shared/Icon.tsx
@@ -13,21 +13,23 @@ type IconInterface = React.FunctionComponent<{
 const Icon: IconInterface = ({ icon, style = 'brands', href, size, className }) => {
 	const handleProviderLogin = async (event: React.MouseEvent<HTMLAnchorElement>, provider: Provider) => {
 		event.preventDefault();
-		await supabase.auth.signIn(
-			{
-				provider,
-			},
-			{
-				redirectTo: 'https://example.com/welcome',
-			}
-		);
+		await supabase.auth.signIn({
+			provider,
+		});
 	};
+
+	if (href) {
+		return (
+			<a href={href} className={`${className} cursor-pointer drop-shadow`}>
+				<i className={`fa-${style} fa-${icon} fa-${size}`} />
+			</a>
+		);
+	}
 
 	return (
 		<a
-			href={href}
 			onClick={(event) => handleProviderLogin(event, icon as Provider)}
-			className={`${className} px-2 py-1 border rounded-full shadow cursor-pointer hover:border-teal-500`}
+			className={`${className} px-2 py-1 border rounded-full drop-shadow cursor-pointer hover:border-teal-500`}
 		>
 			<i className={`fa-${style} fa-${icon} fa-${size}`} />
 		</a>
