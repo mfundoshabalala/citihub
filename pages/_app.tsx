89a2fbeea620bc17a61961ff1fbd01953c8219f6
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-05 13:33:15 +0200

    per-page layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import '../styles/globals.css';
 //
-import { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 //
 import { AuthProvider } from 'lib/auth';
 import { supabase } from 'lib/client';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
+type NextPageWithLayout = NextPage & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component?.getLayout ?? ((page) => page);
+
+	return getLayout(
 		<AuthProvider supabase={supabase}>
 			<Component {...pageProps} />
 		</AuthProvider>
