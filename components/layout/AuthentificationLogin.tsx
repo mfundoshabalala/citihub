1b574d7d21a5aa22af0adbf38109e0348f07d22f
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-24 13:59:54 +0200

    refactoring

--- components/layout/AuthentificationLogin.tsx
+++ components/layout/AuthentificationLogin.tsx
@@ -12,7 +12,10 @@ const AuthentificationLogin = () => {
 	return (
 		<AuthentificationContainer>
 			<AuthentificationHeader title="CapaCiTi Hub" subtitle="Login to your account." />
-			<AuthentificationForm page="Login" />
+			<AuthentificationForm
+				page="Login"
+				className="font-medium shadow-lg active:shadow-sm transition-all duration-300 border-0 active:scale-[0.98] text-white bg-gradient-to-r from-teal-800 via-teal-700 to-teal-800"
+			/>
 			<AuthentificationProviderPanel />
 			<AuthentificationFooter heading="Not a registered member?" href="/register" title="register" />
 		</AuthentificationContainer>
--- components/shared/AuthentificationFooter.tsx
+++ components/shared/AuthentificationFooter.tsx
@@ -3,15 +3,9 @@ import React from 'react';
 // component import
 import LocalLink from './LocalLink';
 
-type AuthentificationFooterInterface = React.FunctionComponent<{
-	heading: string;
-	title: string;
-	href: string;
-}>;
-
 const AuthentificationFooter: AuthentificationFooterInterface = ({ heading, title, href }) => {
 	return (
-		<footer className="flex items-center justify-center px-4 text-sm italic gap-x-2">
+		<footer className="flex items-center justify-center px-4 text-sm gap-x-2">
 			<span className="">{heading}</span>
 			<LocalLink href={href} title={title} />
 		</footer>
