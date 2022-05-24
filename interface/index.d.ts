eac126ab85c043f89dce0a9020c947bfc2394e87
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-24 12:53:08 +0200

    custom types commit

--- interface/index.d.ts
+++ interface/index.d.ts
@@ -1,1 +1,23 @@
 declare module 'next/server';
+
+type ChildrenInterface = React.FunctionComponent<{
+	children: React.ReactNode;
+}>;
+
+type LocalLinkInterface = React.FunctionComponent<{
+	href: string;
+	title: string;
+	className?: string;
+	prefetch?: boolean;
+}>;
+
+type ButtonInterface = React.FunctionComponent<{
+	title: string;
+	className?: string;
+	type?: 'button' | 'submit';
+	onClick?: () => void;
+}>;
+
+type LinksButtonPanelInterface = ChildrenInterface & { Link: LocalLinkInterface } & {
+	Button: ButtonInterface;
+};
